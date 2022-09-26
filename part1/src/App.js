18a295871fed2b7e812e4c8756c4521f66662e2a
Oliver <oliver.iglesias@vitaance.com>
2022-09-26 22:40:03 +0200

    Exercise 1.2: course information, step2

--- part1/src/App.js
+++ part1/src/App.js
@@ -9,16 +9,16 @@ const Header = (props) => {
 const Content = (props) => {
   return (
   <div>
-    <p>
-      {props.part1} {props.exercises1}
-    </p>
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
+    <Part part={props.exercises.part1} exercise={props.exercises.exercises1}/>
+    <Part part={props.exercises.part2} exercise={props.exercises.exercises2}/>
+    <Part part={props.exercises.part3} exercise={props.exercises.exercises3}/>
   </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercise}</p>
   )
 }
 
@@ -45,9 +45,7 @@ const App = () => {
   return (
       <div>
           <Header course={course} />
-          <Content part1={part1} exercises1={exercises1}/>
-          <Content part2={part2} exercises2={exercises2}/>
-          <Content part3={part3} exercises3={exercises3}/>
+          <Content exercises={{part1:part1, exercises1:exercises1, part2:part2, exercises2:exercises2, part3:part3, exercises3:exercises3}}/>
           <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
       </div>
   )
